b03cecc463fc5b4f3d971c9b0bb05c47882421b9
AAV-99 <andres.arteagav@hotmail.com>
2026-09-25 11:25:24 -0500

    Cambio a modelo Ollama local en Hefesto

--- src/vdi/crews/vdicrew/vdicrew.py
+++ src/vdi/crews/vdicrew/vdicrew.py
@@ -10,7 +10,7 @@ class VdiCrew:
     tasks_config = "config/tasks.yaml"
 
     llm = LLM(
-        model="ollama/qwen3.8:27b",  # Tag exacto de tu modelo
+        model="ollama/qwen3.8:latest",  # Tag exacto de tu modelo
         base_url="http://localhost:11434",  # Cambia por la IP de Hefesto si ejecutas el script de forma remota
         temperature= 0.2,  # Temperatura baja para mayor consistencia en diseño técnico
     )
